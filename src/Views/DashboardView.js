1ed2b0cec59dd9bf0722352b04c47205820e3590
Ashley Kircher <ashleywkircher@gmail.com>
2021-02-17 10:16:02 -0600

    Dashboard view has a loading state and checks if there's a user logged in or not

--- src/Views/DashboardView.js
+++ src/Views/DashboardView.js
@@ -1,8 +1,7 @@
 import { UserContext } from "../Models/UserContext"
 import { BoardView } from "./BoardView";
 import { api } from "../API/api"
-import { useContext, useState } from "react"
-import { useEffect } from "react"
+import { useContext, useState, useEffect } from "react"
 import {
     BrowserRouter as Router,
     Link,
@@ -11,63 +10,61 @@ import {
   } from "react-router-dom";
 
 export const DashboardView = function() {
+    const [isLoading, setIsLoading] = useState(true)
     const user = useContext(UserContext)
-    const username = user.username;
-    const userId = user.uid;
     const remote = api()
-
-    // An array of board uid's from the user object. 
-    // Empty if user hasn't been added to context yet.
-    const [boards, setBoards] = useState([])
+    const [ownedBoards, setOwnedBoards] = useState([])
+    const [sharedBoards, setSharedBoards] = useState([])
     
-    const initBoards = async function(boards) {
-        if (boards.length === 0) {
+    const initBoards = async function(user) {
+        if (!user) {
             return;
         }
-        let response = await remote.getBoardsByUserId(userId);
+        let response = await remote.getBoardsByUserId(user.uid);
         if (response) {
-            setBoards(response)
+            setOwnedBoards(response.filter((board) => board.ownerId === user.uid))
+            setSharedBoards(response.filter((board) => board.ownerId !== user.uid))
+            setIsLoading(false)
         } else {
             console.log('not found')
         }
     };
-
-    useEffect(() => {
-        initBoards(userId);
-    },[])
-
-    const ownedBoards = boards.filter((board) => board.ownerId === userId)
-    const sharedBoards = boards.filter((board) => board.ownerId !== userId)
-
+    
     const ownedBoardsLinks = ownedBoards.map((board) =>
-        <div key={`owned${board.uid}`}>
+    <div key={`owned${board.uid}`}>
             <Link to={`/board/${board.uid}`}>{board.name}</Link>
         </div>
     );
     const sharedBoardsLinks = sharedBoards.map((board) => 
-        <div key={`shared${board.uid}`}>
+    <div key={`shared${board.uid}`}>
             <Link to={`/board/${board.uid}`}>{board.name}</Link>
         </div>
     );
-
-    console.log(boards)
-    // add the conditional loading stuff here -- so either return what's written
-    // now, OR if no data then return some other thing
-    return (
-        <div>
-            <h1>Hi {username}!</h1>
-            <Router>
-                <Switch>
-                    <Route exact path="/dashboard">
-                        <h2>Your Boards</h2>
-                        {ownedBoardsLinks}
-                        <h2>Shared Boards</h2>
-                        {sharedBoardsLinks}
-                    </Route>
-                    <Route exact path="/board/:boardId" component={BoardView}></Route>
-                </Switch>
-            </ Router>
-        </div>
-
-    )
+    
+    useEffect(() => {
+        initBoards(user);
+    },[])
+    
+    if (isLoading) {
+        return (
+            <div>please hold!</div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Hi {user.username}!</h1>
+                <Router>
+                    <Switch>
+                        <Route exact path="/dashboard">
+                            <h2>Your Boards</h2>
+                            {ownedBoardsLinks}
+                            <h2>Shared Boards</h2>
+                            {sharedBoardsLinks}
+                        </Route>
+                        <Route exact path="/board/:boardId" component={BoardView}></Route>
+                    </Switch>
+                </ Router>
+            </div>
+        )
+    }
 }
