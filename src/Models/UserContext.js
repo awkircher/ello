4dd2f455dd76a4b5bf1fedd4722c6a915231166e
Ashley Kircher <ashleywkircher@gmail.com>
2021-02-17 09:28:59 -0600

    UserContext now shows null if there is not user logged in yet

--- src/Models/UserContext.js
+++ src/Models/UserContext.js
@@ -1,8 +1,5 @@
 import { createContext } from 'react';
 
 export const UserContext = createContext({
-  "uid":"uid unknown",
-  "username":"username unknown",
-  "password":"password unknown",
-  "boards": []
+  default: null
 });
