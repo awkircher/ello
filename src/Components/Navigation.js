2496e8b33fd0d19a726758c2216eef2c4d74bc17
Ashley Kircher <ashleywkircher@gmail.com>
2021-02-22 12:01:37 -0600

    Additional styling for login screen, navigation bar, and updated logo

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,26 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom'
 import { useContext } from 'react'
 import { UserContext } from '../Models/UserContext'
+import './Navigation.css'
 
 export const Navigation = function(props) {
     const user = useContext(UserContext)
+    const match = useRouteMatch();
+    const isDashboard = match.url.includes('/boards')
+
     return (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">LOGO</Link>
-            </li>
-            <li>
-                <Link to={`/${user.currentUser.username}/boards`}>HOME ICON</Link>
-            </li>
-            <li>
-                <Link to="/login">
-                    <button onClick={() => {user.setLogOut(user.isLoggedIn);}}>
-                        Log out
-                    </button>
-                </Link>
-            </li>
-          </ul>
+        <nav className={isDashboard ? 'dash' : 'board'}>
+            <Link className="buttonContainer" to={`/${user.currentUser.username}/boards`}>
+                <button className="main">&#8962;</button>
+            </Link>
+            <Link to="/">LOGO</Link>
+            <Link to="/login">
+                <button className="nested" onClick={() => {user.setLogOut(user.isLoggedIn);}}>
+                    Log out
+                </button>
+            </Link>
         </nav>
     )
 }
